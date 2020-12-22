bb5b70d6ec3a57f7099b43a5455c9276e8f40770
Zhengyu Peng <rookie.dev@gmail.com>
2020-12-22 11:35:46 -0500

    clean comments
    Former-commit-id: 27834218ae59061c43bae2521f3db63f0934e2ae

--- rt/scene.pyx
+++ rt/scene.pyx
@@ -1,19 +1,6 @@
 # distutils: language = c++
 # cython: language_level=3
 
-# This script contains classes that define all the parameters for
-# a radar system
-
-# This script requires that `numpy` be installed within the Python
-# environment you are running this script in.
-
-# This file can be imported as a module and contains the following
-# class:
-
-# * Transmitter - A class defines parameters of a radar transmitter
-# * Receiver - A class defines parameters of a radar receiver
-# * Radar - A class defines basic parameters of a radar system
-
 # ----------
 # RadarSimPy - A Radar Simulator Built with Python
 # Copyright (C) 2018 - 2020  Zhengyu Peng
