9584bca95317ce74915bcda273adfa01e86ab323
Zhengyu Peng <rookie.dev@gmail.com>
2021-01-11 22:07:20 -0500

    update phase noise docs
    Former-commit-id: f0236fc6552807d94cefe9045829ef7020fbe62d

--- radar.py
+++ radar.py
@@ -775,149 +775,105 @@ class Radar:
         noise_amplitude_peak = np.sqrt(2) * noise_amplitude_mixer + noise_amp
         return noise_amplitude_peak
 
-    def phase_noise(Sin, Fs, phase_noise_freq, phase_noise_power):
-        #
-        # Oscillator Phase Noise Model
-        #
-        #  INPUT:
-        #     Sin - input COMPLEX signal, mxn matrix, column vectors are signals.
-        #     Fs  - sampling frequency ( in Hz ) of Sin
-        #     phase_noise_freq  - frequencies at which SSB Phase Noise is defined (offset from carrier in Hz)
-        #     phase_noise_power - SSB Phase Noise power ( in dBc/Hz )
-        #     VALIDATION_ON  - 1 - perform validation, 0 - don't perfrom validation
-        #
-        #  OUTPUT:
-        #     Sout - output COMPLEX phase noised signal
-        #
-        #  NOTE:
-        #     Input signal should be complex
-        #
-        #  EXAMPLE ( How to use add_phase_noise ):
-        #         Assume SSB Phase Noise is specified as follows:
-        #      -------------------------------------------------------
-        #      |  Offset From Carrier      |        Phase Noise      |
-        #      -------------------------------------------------------
-        #      |        1   kHz            |        -84  dBc/Hz      |
-        #      |        10  kHz            |        -100 dBc/Hz      |
-        #      |        100 kHz            |        -96  dBc/Hz      |
-        #      |        1   MHz            |        -109 dBc/Hz      |
-        #      |        10  MHz            |        -122 dBc/Hz      |
-        #      -------------------------------------------------------
-        #
-        #      Assume that we have 10000 samples of complex sinusoid of frequency 3 KHz
-        #      sampled at frequency 40MHz:
-        #
-        #       Fc = 3e3; carrier frequency
-        #       Fs = 40e6; sampling frequency
-        #       t = 0:9999;
-        #       S = exp(j*2*pi*Fc/Fs*t); complex sinusoid
-        #
-        #      Then, to produce phase noised signal S1 from the original signal S run follows:
-        #
-        #       Fs = 40e6;
-        #       phase_noise_freq = [ 1e3, 10e3, 100e3, 1e6, 10e6 ]; Offset From Carrier
-        #       phase_noise_power = [ -84, -100, -96, -109, -122 ]; Phase Noise power
-        #       S1 = add_phase_noise( S, Fs, phase_noise_freq, phase_noise_power );
+    def phase_noise(signal, fs, freq, power, validation=False):
+        """
+        Oscillator Phase Noise Model
 
-        # Version 1.0
-        # Alex Bur-Guy, October 2005
-        # alex@wavion.co.il
-        #
-        # Revisions:
-        #       Version 1.5 -   Comments. Validation.
-        #       Version 1.0 -   initial version
+        :param numpy.2darray signal:
+            Input signal
+        :param float fs:
+            Sampling frequency
+        :param numpy.1darray freq:
+            Frequency of the phase noise
+        :param numpy.1darray power:
+            Power of the phase noise
+        :param boolean validation:
+            Validate phase noise
 
-        # NOTES:
-        # 1)  The presented model is a simple VCO phase noise model based on the following consideration:
-        # If the output of an oscillator is given as  V(t) = V0 * cos( w0*t + phi(t) ),
-        # then phi(t)  is defined as the phase noise.  In cases of small noise
-        # sources (a valid assumption in any usable system), a narrowband modulation approximation can
-        # be used to express the oscillator output as:
-        #
-        # V(t) = V0 * cos( w0*t + phi(t) )
-        #
-        #        = V0 * [cos(w0*t)*cos(phi(t)) - sin(w0*t)*sin(phi(t)) ]
-        #
-        #        ~ V0 * [cos(w0*t) - sin(w0*t)*phi(t)]
-        #
-        # This shows that phase noise will be mixed with the carrier to produce sidebands around the carrier.
-        #
-        #
-        # 2) In other words, exp(j*x) ~ (1+j*x) for small x
-        #
-        # 3) Phase noise = 0 dBc/Hz at freq. offset of 0 Hz
-        #
-        # 4) The lowest phase noise level is defined by the input SSB phase noise power at the maximal
-        #    freq. offset from DC. (IT DOES NOT BECOME EQUAL TO ZERO )
-        #
-        # The generation process is as follows:
-        #  First of all we interpolate (in log-scale) SSB phase noise power spectrum in M
-        #  equally spaced points (on the interval [0 Fs/2] including bounds ).
-        #
-        #  After that we calculate required frequency shape of the phase noise by X(m) = sqrt(P(m)*dF(m))
-        #  and after that complement it by the symmetrical negative part of the spectrum.
-        #
-        #  After that we generate AWGN of power 1 in the freq domain and multiply it sample-by-sample to
-        #  the calculated shape
-        #
-        #  Finally we perform  2*M-2 points IFFT to such generated noise
-        #  ( See comments inside the code )
-        #
-        #  0 dBc/Hz
-        #  \                                                          /
-        #   \                                                        /
-        #    \                                                      /
-        #     \P dBc/Hz                                            /
-        #     .\                                                  /
-        #     . \                                                /
-        #     .  \                                              /
-        #     .   \____________________________________________/  /_ This level is defined by the phase_noise_power at the maximal freq. offset from DC defined in phase_noise_freq
-        #     .                                                   \
-        #  |__| _|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__   (N points)
-        #  0   dF                       Fs/2                          Fs
-        #  DC
-        #
-        #
-        #  For some basics about Oscillator phase noise see:
-        #     http://www.circuitsage.com/pll/plldynamics.pdf
-        #
-        #     http://www.wj.com/pdf/technotes/LO_phase_noise.pdf
+        :return:
+            Signal with phase noise
+        :rtype: numpy.2darray
 
-        # Sin = 1j * np.ones((128, 256))
-        # Fs = 2e6
-        # phase_noise_freq = np.array([1, 10, 100, 1000, 10000])*1000
-        # phase_noise_power = np.array([-84, -100, -96, -109, -122])
+        **NOTES**
 
-        # if max(phase_noise_freq) >= Fs/2
-        #      error( 'Maximal frequency offset should be less than Fs/2');
-        # end
+        - The presented model is a simple VCO phase noise model based
+        on the following consideration:
+            If the output of an oscillator is given as
+            V(t) = V0 * cos( w0*t + phi(t) ), then phi(t) is defined
+            as the phase noise.  In cases of small noise sources (a valid
+            assumption in any usable system), a narrowband modulatio
+            approximation can be used to express the oscillator output as:
 
-        # Make sure phase_noise_freq and  phase_noise_power are the row vectors
+            V(t) = V0 * cos( w0*t + phi(t) )
+                = V0 * [cos(w0*t)*cos(phi(t)) - signal(w0*t)*signal(phi(t)) ]
+                ~ V0 * [cos(w0*t) - signal(w0*t)*phi(t)]
 
-        # if length( phase_noise_freq ) ~= length( phase_noise_power )
-        #      error('phase_noise_freq and phase_noise_power should be of the same length');
-        # end
+            This shows that phase noise will be mixed with the carrier
+            to produce sidebands around the carrier.
 
-        # Sort phase_noise_freq and phase_noise_power
-        sort_idx = np.argsort(phase_noise_freq)
-        phase_noise_freq = phase_noise_freq[sort_idx]
-        phase_noise_power = phase_noise_power[sort_idx]
+        - In other words, exp(j*x) ~ (1+j*x) for small x
 
-        cut_idx = np.where(phase_noise_freq < Fs/2)
-        phase_noise_freq = phase_noise_freq[cut_idx]
-        phase_noise_power = phase_noise_power[cut_idx]
+        - Phase noise = 0 dBc/Hz at freq. offset of 0 Hz
+
+        - The lowest phase noise level is defined by the input SSB phase
+        noise power at the maximal freq. offset from DC.
+        (IT DOES NOT BECOME EQUAL TO ZERO )
+
+        The generation process is as follows:
+
+        First of all we interpolate (in log-scale) SSB phase noise power
+        spectrum in M equally spaced points
+        (on the interval [0 fs/2] including bounds ).
+
+        After that we calculate required frequency shape of the phase
+        noise by X(m) = sqrt(P(m)*dF(m)) and after that complement it
+        by the symmetrical negative part of the spectrum.
+
+        After that we generate AWGN of power 1 in the freq domain and
+        multiply it sample-by-sample to the calculated shape
+
+        Finally we perform  2*M-2 points IFFT to such generated noise
+
+        ::
+
+            |  0 dBc/Hz
+            |  \                                                    /
+            |   \                                                  /
+            |    \                                                /
+            |     \P dBc/Hz                                      /
+            |     .\                                            /
+            |     . \                                          /
+            |     .  \                                        /
+            |     .   \______________________________________/ <- This level
+            |     .              is defined by the power at the maximal freq
+            |  |__| _|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__  (N)
+            |  0   dF                    fs/2                       fs
+            |  DC
+            |
+        """
+
+        signal = signal.astype(complex)
+
+        # Sort freq and power
+        sort_idx = np.argsort(freq)
+        freq = freq[sort_idx]
+        power = power[sort_idx]
+
+        cut_idx = np.where(freq < fs/2)
+        freq = freq[cut_idx]
+        power = power[cut_idx]
 
         # Add 0 dBc/Hz @ DC
-        if not np.any(np.isin(phase_noise_freq, 0)):
-            phase_noise_freq = np.concatenate(([0], phase_noise_freq))
-            phase_noise_power = np.concatenate(([0], phase_noise_power))
+        if not np.any(np.isin(freq, 0)):
+            freq = np.concatenate(([0], freq))
+            power = np.concatenate(([0], power))
 
         # Calculate input length
-        [numCol, N] = np.shape(Sin)
+        [row, N] = np.shape(signal)
         # Define M number of points (frequency resolution) in the positive spectrum
-        #  (M equally spaced points on the interval [0 Fs/2] including bounds),
+        #  (M equally spaced points on the interval [0 fs/2] including bounds),
         # then the number of points in the negative spectrum will be M-2
-        #  ( interval (Fs/2, Fs) not including bounds )
+        #  ( interval (fs/2, fs) not including bounds )
         #
         # The total number of points in the frequency domain will be 2*M-2, and if we want
         #  to get the same length as the input signal, then
@@ -933,33 +889,33 @@ class Radar:
             M = int(N/2 + 1)
 
         # Equally spaced partitioning of the half spectrum
-        F = np.linspace(0, Fs/2, int(M))    # Freq. Grid
+        F = np.linspace(0, fs/2, int(M))    # Freq. Grid
         dF = np.concatenate((np.diff(F), [F[-1]-F[-2]]))  # Delta F
 
         realmin = np.finfo(float).tiny
 
-        # Perform interpolation of phase_noise_power in log-scale
-        intrvlNum = len(phase_noise_freq)
+        # Perform interpolation of power in log-scale
+        intrvlNum = len(freq)
         logP = np.zeros(int(M))
         # for intrvlIndex = 1 : intrvlNum,
         for intrvlIndex in range(0, intrvlNum):
-            leftBound = phase_noise_freq[intrvlIndex]
-            t1 = phase_noise_power[intrvlIndex]
+            leftBound = freq[intrvlIndex]
+            t1 = power[intrvlIndex]
             if intrvlIndex == intrvlNum-1:
-                rightBound = Fs/2
-                t2 = phase_noise_power[-1]
+                rightBound = fs/2
+                t2 = power[-1]
                 inside = np.where(np.logical_and(
                     F >= leftBound, F <= rightBound))
             else:
-                rightBound = phase_noise_freq[intrvlIndex+1]
-                t2 = phase_noise_power[intrvlIndex+1]
+                rightBound = freq[intrvlIndex+1]
+                t2 = power[intrvlIndex+1]
                 inside = np.where(np.logical_and(
                     F >= leftBound, F < rightBound))
 
             logP[inside] = t1 + (np.log10(F[inside] + realmin) - np.log10(leftBound + realmin)) / (
                 np.log10(rightBound + 2*realmin) - np.log10(leftBound + realmin)) * (t2-t1)
 
-        # Interpolated P ( half spectrum [0 Fs/2] ) [ dBc/Hz ]
+        # Interpolated P ( half spectrum [0 fs/2] ) [ dBc/Hz ]
         P = 10**(np.real(logP)/10)
 
         # Now we will generate AWGN of power 1 in frequency domain and shape it by the desired shape
@@ -984,23 +940,23 @@ class Radar:
 
         # Generate AWGN of power 1
 
-        awgn_P1 = (np.sqrt(0.5)*(np.random.randn(numCol, M) +
-                                 1j*np.random.randn(numCol, M)))
-    #     awgn_P1 = (np.sqrt(0.5)*(np.ones((numCol, M)) +
-    #                              1j*np.ones((numCol, M))))
+        if validation:
+            awgn_P1 = (np.sqrt(0.5)*(np.ones((row, M)) +
+                                     1j*np.ones((row, M))))
+        else:
+            awgn_P1 = (np.sqrt(0.5)*(np.random.randn(row, M) +
+                                     1j*np.random.randn(row, M)))
 
-        # Shape the noise on the positive spectrum [0, Fs/2] including bounds ( M points )
+        # Shape the noise on the positive spectrum [0, fs/2] including bounds ( M points )
         X = (2*M-2) * np.sqrt(dF * P) * awgn_P1
 
         # X = np.transpose(X)
-        # Complete symmetrical negative spectrum  (Fs/2, Fs) not including bounds (M-2 points)
-        tmp_X = np.zeros((numCol, int(M*2-2)), dtype=complex)
+        # Complete symmetrical negative spectrum  (fs/2, fs) not including bounds (M-2 points)
+        tmp_X = np.zeros((row, int(M*2-2)), dtype=complex)
         tmp_X[:, 0:M] = X
         tmp_X[:, M:(2*M-2)] = np.fliplr(np.conjugate(X[:, 1:-1]))
 
         X = tmp_X
-
-        # X( M + (1:M-2),: ) = flipud( conj(X(2:end-1,:)) );
 
         # Remove DC
         X[:, 0] = 0
@@ -1010,10 +966,6 @@ class Radar:
 
         # Calculate phase noise
         phase_noise = np.exp(1j * np.real(x_t[:, 0:N]))
-    #     phase_noise = x[:, 0:N]
-        # phase_noise = 1 + 1i*real(x(1:N));
 
         # Add phase noise
-        Sout = Sin * phase_noise
-
-        return Sout
+        return signal * phase_noise
