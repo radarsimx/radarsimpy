da6adfe2946c4e50b24eb3d700694f6e64605be1
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2021-02-02 13:47:19 -0500

    Update scene.pyx
    Former-commit-id: fa46e96fd93bf4b82a9b469a460a3d46b3ad309a

--- rt/scene.pyx
+++ rt/scene.pyx
@@ -316,10 +316,10 @@ cpdef scene(radar, targets, correction=0, density=10, level=None, noise=True):
             rays[count]['polarization'][1] = snaps[snapshot_idx].ray_received[idx].pol_[1]
             rays[count]['polarization'][2] = snaps[snapshot_idx].ray_received[idx].pol_[2]
             rays[count]['path_pos'] = np.zeros((20,3))
-            for path_idx in range(0, <int_t>(rays[count]['refCount']+2)):
-                rays[count]['path_pos'][path_idx, 0] = snaps[snapshot_idx].ray_received[idx].path_[path_idx].loc_[0]
-                rays[count]['path_pos'][path_idx, 1] = snaps[snapshot_idx].ray_received[idx].path_[path_idx].loc_[1]
-                rays[count]['path_pos'][path_idx, 2] = snaps[snapshot_idx].ray_received[idx].path_[path_idx].loc_[2]
+            # for path_idx in range(0, <int_t>(rays[count]['refCount']+2)):
+            #     rays[count]['path_pos'][path_idx, 0] = snaps[snapshot_idx].ray_received[idx].path_[path_idx].loc_[0]
+            #     rays[count]['path_pos'][path_idx, 1] = snaps[snapshot_idx].ray_received[idx].path_[path_idx].loc_[1]
+            #     rays[count]['path_pos'][path_idx, 2] = snaps[snapshot_idx].ray_received[idx].path_[path_idx].loc_[2]
             count=count+1
 
 
