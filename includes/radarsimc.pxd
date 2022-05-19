99a30d6034295e5332a4fbe1fda2bee06f8deef9
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-05-19 10:58:46 -0400

    Revert "use float for snapshot"
    This reverts commit 852d726cea404fd18f205d8586c4057cbedf869b [formerly f4e2b77bb9869b3d9f2f753bdfe471d861130b34].
    
    
    Former-commit-id: 392a41600afdb282bc14c2add854b9cf1d036b23

--- includes/radarsimc.pxd
+++ includes/radarsimc.pxd
@@ -243,7 +243,7 @@ cdef extern from "scene.hpp":
         void SetRadar(const Radar[F]& radar)
         void RunSimulator(int level,
                           bool debug,
-                          vector[Snapshot[F]]& snapshots,
+                          vector[Snapshot[T]]& snapshots,
                           double* bb_real,
                           double* bb_imag)
 
--- rt/scene.pyx
+++ rt/scene.pyx
@@ -267,6 +267,7 @@ cpdef scene(radar, targets, density=1, level=None, noise=True, debug=False):
     Snapshot
     """
     cdef vector[Snapshot[float_t]] snaps
+    cdef vector[Snapshot[double]] snaps_double
     cdef int level_id
 
     if level is None:
@@ -286,8 +287,8 @@ cpdef scene(radar, targets, density=1, level=None, noise=True, debug=False):
         for fm_idx in range(0, frames):
             for tx_idx in range(0, tx_ch):
                 for ps_idx in range(0, pulses):
-                    snaps.push_back(
-                        Snapshot[float_t](
+                    snaps_double.push_back(
+                        Snapshot[double](
                         timestamp[fm_idx*total_ch+tx_idx*rx_ch, ps_idx, 0],
                         fm_idx,
                         tx_idx,
@@ -300,8 +301,8 @@ cpdef scene(radar, targets, density=1, level=None, noise=True, debug=False):
             for tx_idx in range(0, tx_ch):
                 for ps_idx in range(0, pulses):
                     for sp_idx in range(0, samples):
-                        snaps.push_back(
-                            Snapshot[float_t](
+                        snaps_double.push_back(
+                            Snapshot[double](
                             timestamp[fm_idx*total_ch+tx_idx*rx_ch, ps_idx, sp_idx],
                             fm_idx,
                             tx_idx,
@@ -322,7 +323,7 @@ cpdef scene(radar, targets, density=1, level=None, noise=True, debug=False):
             level_id, debug, snaps, bb_real, bb_imag)
     else:
         radar_scene_double.RunSimulator(
-            level_id, debug, snaps, bb_real, bb_imag)
+            level_id, debug, snaps_double, bb_real, bb_imag)
 
     baseband = np.zeros((frames*total_ch, pulses, samples), dtype=complex)
 
