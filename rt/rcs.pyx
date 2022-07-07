ec01f2993510602386319d740306a105d09e37cb
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2022-07-07 14:27:24 -0400

    Update rcs.pyx
    Former-commit-id: 8cac9e4e45426b3831e1ad253ad7c42faff5793c

--- rt/rcs.pyx
+++ rt/rcs.pyx
@@ -33,33 +33,47 @@ from radarsimpy.includes.type_def cimport int_t
 from radarsimpy.includes.radarsimc cimport Target, Rcs
 import meshio
 import numpy as np
-cimport cython
 
+cimport cython
 cimport numpy as np
-# from stl import mesh
 
 
 @cython.cdivision(True)
 @cython.boundscheck(False)
 @cython.wraparound(False)
-cpdef rcs_sbr(model, f, obs_phi, obs_theta, inc_phi=None, inc_theta=None, pol=[0, 0, 1], density=1):
+cpdef rcs_sbr(model,
+              f,
+              obs_phi,
+              obs_theta,
+              inc_phi=None,
+              inc_theta=None,
+              pol=[0, 0, 1],
+              density=1):
     """
     rcs_sbr(model, f, obs_phi, obs_theta, inc_phi=None, inc_theta=None, pol=[0, 0, 1], density=1)
 
-    Calculate target's RCS by using raytracing
+    Calculate target's RCS by using shooting and bouncing rays (SBR)
 
     :param str model:
-        Path of the model
-    :param float phi:
-        Observation angle phi (deg)
-    :param float theta:
-        Observation angle theta (deg)
+        Path of the model file
     :param float f:
         Center frequency (Hz)
+    :param float obs_phi:
+        Observation angle phi (deg)
+    :param float obs_theta:
+        Observation angle theta (deg)
+    :param float inc_phi:
+        Incidence angle phi (deg).
+        ``default None`` means ``inc_phi = obs_phi``
+    :param float inc_theta:
+        Incidence angle theta (deg).
+        ``default None`` means ``inc_theta = obs_theta``
     :param list pol:
-        Polarization [x, y, z]. ``default [0, 0, 1]``
+        Polarization [x, y, z].
+        ``default [0, 0, 1]``
     :param float density:
-        Ray density (number of rays per wavelength). ``default 10``
+        Ray density (number of rays per wavelength).
+        ``default 1``
 
     :return: Target's RCS (m^2), use 10*log10(RCS) to convert to dBsm
     :rtype: float
@@ -80,22 +94,22 @@ cpdef rcs_sbr(model, f, obs_phi, obs_theta, inc_phi=None, inc_theta=None, pol=[0
     obs_theta_rad = np.radians(obs_theta)
 
     cdef Vec3[double] inc_dir = Vec3[double](
-        < double > (np.sin(inc_theta_rad)*np.cos(inc_phi_rad)),
-        < double > (np.sin(inc_theta_rad)*np.sin(inc_phi_rad)),
-        < double > (np.cos(inc_theta_rad)))
+        <double> (np.sin(inc_theta_rad)*np.cos(inc_phi_rad)),
+        <double> (np.sin(inc_theta_rad)*np.sin(inc_phi_rad)),
+        <double> (np.cos(inc_theta_rad)))
 
     cdef Vec3[double] obs_dir = Vec3[double](
-        < double > (np.sin(obs_theta_rad)*np.cos(obs_phi_rad)),
-        < double > (np.sin(obs_theta_rad)*np.sin(obs_phi_rad)),
-        < double > (np.cos(obs_theta_rad)))
+        <double> (np.sin(obs_theta_rad)*np.cos(obs_phi_rad)),
+        <double> (np.sin(obs_theta_rad)*np.sin(obs_phi_rad)),
+        <double> (np.cos(obs_theta_rad)))
 
     cdef Rcs[double] rcs
 
-    rcs = Rcs[double](Target[float]( & points[0, 0], & cells[0, 0], < int_t > cells.shape[0]),
+    rcs = Rcs[double](Target[float](& points[0, 0], & cells[0, 0], < int_t > cells.shape[0]),
                       inc_dir,
                       obs_dir,
-                      Vec3[double]( < double > pol[0], < double > pol[1], < double > pol[2]),
-                      < double > f,
-                      < double > density)
+                      Vec3[double](<double> pol[0], <double> pol[1], <double> pol[2]),
+                      <double> f,
+                      <double> density)
 
     return rcs.CalculateRcs()
