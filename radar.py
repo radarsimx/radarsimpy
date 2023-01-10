102772a2d73cae87ace64e699267ce233619c770
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2023-01-09 20:58:25 -0500

    adding interference
    Former-commit-id: 4f64c7a8eb967829c96f7d86ac8d190271c4646b
    Former-commit-id: d08f97d78c8f5af7246adfc69cce13a037a3b735
    Former-commit-id: b4fafee9bb14df505c8734430e30b36879d2e273

--- radar.py
+++ radar.py
@@ -704,6 +704,7 @@ class Radar:
                  rotation=(0, 0, 0),
                  rotation_rate=(0, 0, 0),
                  time=0,
+                 interference=None,
                  seed=None,
                  **kwargs):
 
@@ -938,6 +939,17 @@ class Radar:
             self.loccation = np.array(location)
             self.rotation = np.array(np.radians(rotation))
             self.rotation_rate = np.array(np.radians(rotation_rate))
+
+        if interference is not None:
+            self.int_f = interference.f
+            self.int_t = interference.t
+            self.int_k = interference.k
+
+            self.int_phs = np.zeros_like(self.int_f)
+            self.int_phs = self.int_f*interference.delta_t + 0.5 * \
+                self.int_k*interference.delta_t*interference.delta_t
+            self.int_phs = np.cumsum(self.int_phs)
+            self.int_pulse_start = interference.transmitter.pulse_start_time
 
     def gen_timestamp(self):
         """
