9b62e6ae054edaeea329415befdde960f684543f
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2023-08-14 09:17:38 -0400

    Update conf.py

--- gen_docs/conf.py
+++ gen_docs/conf.py
@@ -41,6 +41,8 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
+html_favicon = 
+
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
 # This pattern also affects html_static_path and html_extra_path.
