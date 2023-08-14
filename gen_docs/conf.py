708dd1a34e7f60681b8a342529d682a5fb2497b1
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2023-08-14 09:18:54 -0400

    Update conf.py

--- gen_docs/conf.py
+++ gen_docs/conf.py
@@ -41,7 +41,7 @@ extensions = [
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
 
-html_favicon = 
+html_favicon = 'https://radarsimx.com/favicon.ico'
 
 # List of patterns, relative to source directory, that match files and
 # directories to ignore when looking for source files.
