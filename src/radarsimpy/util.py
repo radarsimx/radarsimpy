00fc034bc44160c80e9a892d8231ad717fd40d27
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2023-10-26 09:47:00 -0400

    Update util.py

--- src/radarsimpy/util.py
+++ src/radarsimpy/util.py
@@ -79,17 +79,17 @@ def cal_phase_noise(signal, fs, freq, power, seed=None, validation=False):
     The generation process is as follows:
 
     First of all we interpolate (in log-scale) SSB phase noise power
-    spectrum in M equally spaced points
+    spectrum in num_f_points equally spaced points
     (on the interval [0 fs/2] including bounds ).
 
     After that we calculate required frequency shape of the phase
-    noise by X(m) = sqrt(P(m)*dF(m)) and after that complement it
+    noise by spec_noise(m) = sqrt(P(m)*delta_f(m)) and after that complement it
     by the symmetrical negative part of the spectrum.
 
     After that we generate AWGN of power 1 in the freq domain and
     multiply it sample-by-sample to the calculated shape
 
-    Finally we perform  2*M-2 points IFFT to such generated noise
+    Finally we perform  2*num_f_points-2 points IFFT to such generated noise
 
     ::
 
@@ -104,7 +104,7 @@ def cal_phase_noise(signal, fs, freq, power, seed=None, validation=False):
         |    .   \\______________________________________/ <- This level
         |    .              is defined by the power at the maximal freq
         |  |__| _|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__|__  (N)
-        |  0   dF                    fs/2                       fs
+        |  0   delta_f                    fs/2                       fs
         |  DC
         |
     """
@@ -131,112 +131,123 @@ def cal_phase_noise(signal, fs, freq, power, seed=None, validation=False):
         power = np.concatenate(([0], power))
 
     # Calculate input length
-    [row, N] = np.shape(signal)
-    # Define M number of points (frequency resolution) in the
-    # positive spectrum (M equally spaced points on the interval
+    [row, num_samples] = np.shape(signal)
+    # Define num_f_points number of points (frequency resolution) in the
+    # positive spectrum (num_f_points equally spaced points on the interval
     # [0 fs/2] including bounds), then the number of points in the
-    # negative spectrum will be M-2 ( interval (fs/2, fs) not
+    # negative spectrum will be num_f_points-2 ( interval (fs/2, fs) not
     # including bounds )
     #
     # The total number of points in the frequency domain will be
-    # 2*M-2, and if we want to get the same length as the input
+    # 2*num_f_points-2, and if we want to get the same length as the input
     # signal, then
-    #   2*M-2 = N
-    #   M-1 = N/2
-    #   M = N/2 + 1
+    #   2*num_f_points-2 = num_samples
+    #   num_f_points-1 = num_samples/2
+    #   num_f_points = num_samples/2 + 1
     #
-    # So, if N is even then M = N/2 + 1, and if N is odd we will take
-    # M = (N+1)/2 + 1
+    # So, if num_samples is even then num_f_points = num_samples/2 + 1,
+    # and if num_samples is odd we will take
+    # num_f_points = (num_samples+1)/2 + 1
     #
-    if np.remainder(N, 2):
-        M = int((N + 1) / 2 + 1)
+    if np.remainder(num_samples, 2):
+        num_f_points = int((num_samples + 1) / 2 + 1)
     else:
-        M = int(N / 2 + 1)
+        num_f_points = int(num_samples / 2 + 1)
 
     # Equally spaced partitioning of the half spectrum
-    F = np.linspace(0, fs / 2, int(M))  # Freq. Grid
-    dF = np.concatenate((np.diff(F), [F[-1] - F[-2]]))  # Delta F
+    f_grid = np.linspace(0, fs / 2, int(num_f_points))  # Freq. Grid
+    delta_f = np.concatenate((np.diff(f_grid), [f_grid[-1] - f_grid[-2]]))  # Delta F
 
     realmin = np.finfo(np.float64).tiny
     # realmin = 1e-30
 
     # Perform interpolation of power in log-scale
-    intrvlNum = len(freq)
-    logP = np.zeros(int(M))
-    # for intrvlIndex = 1 : intrvlNum,
-    for intrvlIndex in range(0, intrvlNum):
-        leftBound = freq[intrvlIndex]
-        t1 = power[intrvlIndex]
-        if intrvlIndex == intrvlNum - 1:
-            rightBound = fs / 2
+    intrvl_num = len(freq)
+    log_p = np.zeros(int(num_f_points))
+    # for intrvl_index = 1 : intrvl_num,
+    for intrvl_index in range(0, intrvl_num):
+        left_bound = freq[intrvl_index]
+        t1 = power[intrvl_index]
+        if intrvl_index == intrvl_num - 1:
+            right_bound = fs / 2
             t2 = power[-1]
-            inside = np.where(np.logical_and(F >= leftBound, F <= rightBound))
+            inside = np.where(
+                np.logical_and(f_grid >= left_bound, f_grid <= right_bound)
+            )
         else:
-            rightBound = freq[intrvlIndex + 1]
-            t2 = power[intrvlIndex + 1]
-            inside = np.where(np.logical_and(F >= leftBound, F < rightBound))
+            right_bound = freq[intrvl_index + 1]
+            t2 = power[intrvl_index + 1]
+            inside = np.where(
+                np.logical_and(f_grid >= left_bound, f_grid < right_bound)
+            )
 
-        logP[inside] = t1 + (
-            np.log10(F[inside] + realmin) - np.log10(leftBound + realmin)
-        ) / (np.log10(rightBound + 2 * realmin) - np.log10(leftBound + realmin)) * (
+        log_p[inside] = t1 + (
+            np.log10(f_grid[inside] + realmin) - np.log10(left_bound + realmin)
+        ) / (np.log10(right_bound + 2 * realmin) - np.log10(left_bound + realmin)) * (
             t2 - t1
         )
 
     # Interpolated P ( half spectrum [0 fs/2] ) [ dBc/Hz ]
-    P = 10 ** (np.real(logP) / 10)
+    p_interp = 10 ** (np.real(log_p) / 10)
 
     # Now we will generate AWGN of power 1 in frequency domain and shape
     # it by the desired shape as follows:
     #
-    #    At the frequency offset F(m) from DC we want to get power Ptag(m)
-    #    such that P(m) = Ptag/dF(m), that is we have to choose
-    #    X(m) = sqrt( P(m)*dF(m) );
+    #    At the frequency offset f_grid(m) from DC we want to get power Ptag(m)
+    #    such that p_interp(m) = Ptag/delta_f(m), that is we have to choose
+    #    spec_noise(m) = sqrt( p_interp(m)*delta_f(m) );
     #
     # Due to the normalization factors of FFT and IFFT defined as follows:
-    #     For length K input vector x, the DFT is a length K vector X,
+    #     For length K input vector x, the DFT is a length K vector spec_noise,
     #     with elements
     #                K
-    #      X(k) =   sum  x(n)*exp(-j*2*pi*(k-1)*(n-1)/K), 1 <= k <= K.
+    #      spec_noise(k) =   sum  x(n)*exp(-j*2*pi*(k-1)*(n-1)/K), 1 <= k <= K.
     #               n=1
     #     The inverse DFT (computed by IFFT) is given by
     #                      K
-    #      x(n) = (1/K) sum  X(k)*exp( j*2*pi*(k-1)*(n-1)/K), 1 <= n <= K.
+    #      x(n) = (1/K) sum  spec_noise(k)*exp( j*2*pi*(k-1)*(n-1)/K), 1 <= n <= K.
     #                     k=1
     #
-    # we have to compensate normalization factor (1/K) multiplying X(k)
-    # by K. In our case K = 2*M-2.
+    # we have to compensate normalization factor (1/K) multiplying spec_noise(k)
+    # by K. In our case K = 2*num_f_points-2.
 
     # Generate AWGN of power 1
     if validation:
-        awgn_P1 = np.sqrt(0.5) * (np.ones((row, M)) + 1j * np.ones((row, M)))
+        awgn_p1 = np.sqrt(0.5) * (
+            np.ones((row, num_f_points)) + 1j * np.ones((row, num_f_points))
+        )
     else:
-        awgn_P1 = np.sqrt(0.5) * (
-            rng.standard_normal((row, M)) + 1j * rng.standard_normal((row, M))
+        awgn_p1 = np.sqrt(0.5) * (
+            rng.standard_normal((row, num_f_points))
+            + 1j * rng.standard_normal((row, num_f_points))
         )
 
     # Shape the noise on the positive spectrum [0, fs/2] including bounds
-    # ( M points )
-    # X = (2*M-2) * np.sqrt(dF * P) * awgn_P1
-    X = M * np.sqrt(dF * P) * awgn_P1
-    ## NOTE: this normalization should be M vs. (2*M-2) since on line 222 he creates the two-sided spectrum by adding the negative frequency spectrum.
+    # ( num_f_points points )
+    # spec_noise = (2*num_f_points-2) * np.sqrt(delta_f * p_interp) * awgn_p1
+    spec_noise = num_f_points * np.sqrt(delta_f * p_interp) * awgn_p1
+    ## NOTE: this normalization should be num_f_points vs. (2*num_f_points-2)
+    # since on line 222 he creates the two-sided spectrum by adding the negative frequency spectrum.
 
-    # X = np.transpose(X)
+    # spec_noise = np.transpose(spec_noise)
     # Complete symmetrical negative spectrum  (fs/2, fs) not including
-    # bounds (M-2 points)
-    tmp_X = np.zeros((row, int(M * 2 - 2)), dtype=complex)
-    tmp_X[:, 0:M] = X
-    tmp_X[:, M : (2 * M - 2)] = np.fliplr(np.conjugate(X[:, 1:-1]))
+    # bounds (num_f_points-2 points)
+    tmp_spec_noise = np.zeros((row, int(num_f_points * 2 - 2)), dtype=complex)
+    tmp_spec_noise[:, 0:num_f_points] = spec_noise
+    tmp_spec_noise[:, num_f_points : (2 * num_f_points - 2)] = np.fliplr(
+        np.conjugate(spec_noise[:, 1:-1])
+    )
 
-    X = tmp_X
+    spec_noise = tmp_spec_noise
 
     # Remove DC
-    X[:, 0] = 0
+    spec_noise[:, 0] = 0
 
     # Perform IFFT
-    x_t = np.fft.ifft(X, axis=1)
+    x_t = np.fft.ifft(spec_noise, axis=1)
 
     # Calculate phase noise
-    phase_noise = np.exp(-1j * np.real(x_t[:, 0:N]))
+    phase_noise = np.exp(-1j * np.real(x_t[:, 0:num_samples]))
 
     # Add phase noise
     return signal * phase_noise
