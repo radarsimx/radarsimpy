a14809332de72889e1e846851190a5b4f1602a2e
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2023-10-30 15:18:57 -0400

    update

--- src/radarsimpy/transmitter.py
+++ src/radarsimpy/transmitter.py
@@ -1,4 +1,4 @@
-r"""
+"""
 This script contains classes that define all the parameters for
 a radar system
 
@@ -10,17 +10,20 @@ class:
 
 * Transmitter - A class defines parameters of a radar transmitter
 
-----------
-RadarSimPy - A Radar Simulator Built with Python
-Copyright (C) 2018 - PRESENT  radarsimx.com
-E-mail: info@radarsimx.com
-Website: https://radarsimx.com
+---
 
- ____           _            ____  _          __  __
-|  _ \ __ _  __| | __ _ _ __/ ___|(_)_ __ ___ \ \/ /
-| |_) / _` |/ _` |/ _` | '__\___ \| | '_ ` _ \ \  /
-|  _ < (_| | (_| | (_| | |   ___) | | | | | | |/  \
-|_| \_\__,_|\__,_|\__,_|_|  |____/|_|_| |_| |_/_/\_\
+- Copyright (C) 2018 - PRESENT  radarsimx.com
+- E-mail: info@radarsimx.com
+- Website: https://radarsimx.com
+
+::
+
+    ██████╗  █████╗ ██████╗  █████╗ ██████╗ ███████╗██╗███╗   ███╗██╗  ██╗
+    ██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔══██╗██╔════╝██║████╗ ████║╚██╗██╔╝
+    ██████╔╝███████║██║  ██║███████║██████╔╝███████╗██║██╔████╔██║ ╚███╔╝ 
+    ██╔══██╗██╔══██║██║  ██║██╔══██║██╔══██╗╚════██║██║██║╚██╔╝██║ ██╔██╗ 
+    ██║  ██║██║  ██║██████╔╝██║  ██║██║  ██║███████║██║██║ ╚═╝ ██║██╔╝ ██╗
+    ╚═╝  ╚═╝╚═╝  ╚═╝╚═════╝ ╚═╝  ╚═╝╚═╝  ╚═╝╚══════╝╚═╝╚═╝     ╚═╝╚═╝  ╚═╝
 
 """
 
