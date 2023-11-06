ea076feb54ee73e384a47e773dcfe1b1ada1a4eb
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2023-11-06 16:12:02 -0500

    Update __init__.py

--- src/radarsimpy/__init__.py
+++ src/radarsimpy/__init__.py
@@ -1,5 +1,13 @@
 """
-A Python module for radar simulation
+A Radar Simulator for Python
+
+RadarSimPy is a powerful and versatile Python-based Radar Simulator that models
+radar transceivers and simulates baseband data from point targets and 3D models.
+Its signal processing tools offer range/Doppler processing, direction of arrival
+estimation, and beamforming using various cutting-edge techniques, and you can
+even characterize radar detection using Swerling's models. Whether you're a beginner
+or an advanced user, RadarSimPy is the perfect tool for anyone looking to develop
+new radar technologies or expand their knowledge of radar systems.
 
 ---
 
@@ -18,7 +26,7 @@ A Python module for radar simulation
 
 """
 
-from .radar import Radar # pylint: disable=no-name-in-module
+from .radar import Radar  # pylint: disable=no-name-in-module
 from .transmitter import Transmitter
 from .receiver import Receiver
 
