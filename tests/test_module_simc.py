58e583277017f117d75e2517d48484a138688f7f
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-06-13 14:12:19 -0400

    Update test_module_simc.py

--- tests/test_module_simc.py
+++ tests/test_module_simc.py
@@ -430,6 +430,217 @@ def test_simc_radar_moving():
     )
 
 
+def test_simc_2_frames_moving_target():
+    """
+    Basic test case with a single target and simple radar setup.
+    """
+    tx = Transmitter(
+        f=[24.075e9, 24.175e9],
+        t=80e-6,
+        tx_power=10,
+        prp=100e-6,
+        pulses=3,
+        channels=[
+            {
+                "location": (0, 0, 0),
+            }
+        ],
+    )
+    rx = Receiver(
+        fs=6e4,
+        noise_figure=12,
+        rf_gain=20,
+        load_resistor=500,
+        baseband_gain=30,
+        channels=[
+            {
+                "location": (0, 0, 0),
+            }
+        ],
+    )
+    radar = Radar(transmitter=tx, receiver=rx, time=[0, 1])
+
+    targets = [
+        {
+            "location": np.array([10, 0, 0]),
+            "speed": np.array([-5, 0, 0]),
+            "rcs": 20,
+        }
+    ]
+    result = simc(radar, targets, noise=False)
+
+    assert np.allclose(
+        result["baseband"],
+        np.array(
+            [
+                [
+                    [
+                        0.02167872 + 0.01755585j,
+                        -0.02776995 + 0.00265147j,
+                        0.01794177 - 0.02136164j,
+                        0.00216204 + 0.02781327j,
+                    ],
+                    [
+                        0.02746562 + 0.00489594j,
+                        -0.02301209 + 0.01577339j,
+                        0.00536318 - 0.02737916j,
+                        0.01536405 + 0.02328849j,
+                    ],
+                    [
+                        0.02640622 - 0.00901099j,
+                        -0.01251624 + 0.02493704j,
+                        -0.00855785 - 0.0265575j,
+                        0.02473615 + 0.01291073j,
+                    ],
+                ],
+                [
+                    [
+                        0.10501874 + 0.03770757j,
+                        -0.00954112 - 0.11117821j,
+                        -0.09705694 + 0.05506731j,
+                        0.09052647 + 0.06525521j,
+                    ],
+                    [
+                        0.11017892 - 0.01778723j,
+                        -0.06217132 - 0.09268947j,
+                        -0.05829531 + 0.09517927j,
+                        0.11082761 + 0.01324784j,
+                    ],
+                    [
+                        0.08785306 - 0.06886658j,
+                        -0.09926082 - 0.05107721j,
+                        -0.00495222 + 0.11152531j,
+                        0.10337779 - 0.04214599j,
+                    ],
+                ],
+            ]
+        ),
+    )
+
+    assert np.allclose(
+        result["timestamp"],
+        np.array(
+            [
+                [
+                    [0.00000000e00, 1.66666667e-05, 3.33333333e-05, 5.00000000e-05],
+                    [1.00000000e-04, 1.16666667e-04, 1.33333333e-04, 1.50000000e-04],
+                    [2.00000000e-04, 2.16666667e-04, 2.33333333e-04, 2.50000000e-04],
+                ],
+                [
+                    [1.00000000e00, 1.00001667e00, 1.00003333e00, 1.00005000e00],
+                    [1.00010000e00, 1.00011667e00, 1.00013333e00, 1.00015000e00],
+                    [1.00020000e00, 1.00021667e00, 1.00023333e00, 1.00025000e00],
+                ],
+            ]
+        ),
+    )
+
+
+def test_simc_2_frames_moving_radar():
+    """
+    Basic test case with a single target and simple radar setup.
+    """
+    tx = Transmitter(
+        f=[24.075e9, 24.175e9],
+        t=80e-6,
+        tx_power=10,
+        prp=100e-6,
+        pulses=3,
+        channels=[
+            {
+                "location": (0, 0, 0),
+            }
+        ],
+    )
+    rx = Receiver(
+        fs=6e4,
+        noise_figure=12,
+        rf_gain=20,
+        load_resistor=500,
+        baseband_gain=30,
+        channels=[
+            {
+                "location": (0, 0, 0),
+            }
+        ],
+    )
+    radar = Radar(transmitter=tx, receiver=rx, speed=[5, 0, 0], time=[0, 1])
+
+    targets = [
+        {
+            "location": np.array([10, 0, 0]),
+            "speed": np.array([0, 0, 0]),
+            "rcs": 20,
+        }
+    ]
+    result = simc(radar, targets, noise=False)
+
+    assert np.allclose(
+        result["baseband"],
+        np.array(
+            [
+                [
+                    [
+                        0.02167872 + 0.01755585j,
+                        -0.02776995 + 0.00265147j,
+                        0.01794177 - 0.02136164j,
+                        0.00216204 + 0.02781327j,
+                    ],
+                    [
+                        0.02746562 + 0.00489594j,
+                        -0.02301209 + 0.01577339j,
+                        0.00536318 - 0.02737916j,
+                        0.01536405 + 0.02328849j,
+                    ],
+                    [
+                        0.02640622 - 0.00901099j,
+                        -0.01251624 + 0.02493704j,
+                        -0.00855785 - 0.0265575j,
+                        0.02473615 + 0.01291073j,
+                    ],
+                ],
+                [
+                    [
+                        0.10501874 + 0.03770757j,
+                        -0.00954112 - 0.11117821j,
+                        -0.09705694 + 0.05506731j,
+                        0.09052647 + 0.06525521j,
+                    ],
+                    [
+                        0.11017892 - 0.01778723j,
+                        -0.06217132 - 0.09268947j,
+                        -0.05829531 + 0.09517927j,
+                        0.11082761 + 0.01324784j,
+                    ],
+                    [
+                        0.08785306 - 0.06886658j,
+                        -0.09926082 - 0.05107721j,
+                        -0.00495222 + 0.11152531j,
+                        0.10337779 - 0.04214599j,
+                    ],
+                ],
+            ]
+        ),
+    )
+
+    assert np.allclose(
+        result["timestamp"],
+        np.array(
+            [
+                [
+                    [0.00000000e00, 1.66666667e-05, 3.33333333e-05, 5.00000000e-05],
+                    [1.00000000e-04, 1.16666667e-04, 1.33333333e-04, 1.50000000e-04],
+                    [2.00000000e-04, 2.16666667e-04, 2.33333333e-04, 2.50000000e-04],
+                ],
+                [
+                    [1.00000000e00, 1.00001667e00, 1.00003333e00, 1.00005000e00],
+                    [1.00010000e00, 1.00011667e00, 1.00013333e00, 1.00015000e00],
+                    [1.00020000e00, 1.00021667e00, 1.00023333e00, 1.00025000e00],
+                ],
+            ]
+        ),
+    )
+
 def test_simc_multiple_targets():
     """
     Test with multiple targets.
@@ -512,62 +723,6 @@ def test_simc_multiple_targets():
             ]
         ),
     )
-
-
-# def test_simc_different_angles():
-#     """
-#     Test with different radar angles.
-#     """
-#     radar = Radar(
-#         location=np.array([0, 0, 0]),
-#         speed=np.array([0, 0, 0]),
-#         rotation=np.array([45, 30, 0]),
-#         rotation_rate=np.array([0, 0, 0]),
-#         frequency=1e9,
-#         bandwidth=1e6,
-#         pulses=10,
-#         samples_per_pulse=100,
-#         frame_size=1,
-#         rxchannel_prop={"size": 1},
-#         txchannel_prop={"size": 1},
-#     )
-#     targets = [
-#         {
-#             "location": np.array([10, 0, 0]),
-#             "rcs": 0,
-#         }
-#     ]
-#     result = simc(radar, targets)
-#     assert "baseband" in result
-#     assert "timestamp" in result
-#     assert result["baseband"].shape == (1, 10, 100)  # Check baseband shape
-
-# def test_simc_noise():
-#     """
-#     Test with noise enabled and disabled.
-#     """
-#     radar = Radar(
-#         location=np.array([0, 0, 0]),
-#         speed=np.array([0, 0, 0]),
-#         rotation=np.array([0, 0, 0]),
-#         rotation_rate=np.array([0, 0, 0]),
-#         frequency=1e9,
-#         bandwidth=1e6,
-#         pulses=10,
-#         samples_per_pulse=100,
-#         frame_size=1,
-#         rxchannel_prop={"size": 1},
-#         txchannel_prop={"size": 1},
-#     )
-#     targets = [
-#         {
-#             "location": np.array([10, 0, 0]),
-#             "rcs": 0,
-#         }
-#     ]
-#     result_noise = simc(radar, targets, noise=True)
-#     result_no_noise = simc(radar, targets, noise=False)
-#     assert not np.allclose(result_noise["baseband"], result_no_noise["baseband"])
 
 
 # def test_sim_cw():
