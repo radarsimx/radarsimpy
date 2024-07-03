59a5fa52be276c3d0b34bacb3a4d59de6e2173c5
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2024-07-02 23:42:24 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -65,16 +65,16 @@ else:
 os_type = platform.system()  # 'Linux', 'Windows', 'macOS'
 
 if os_type == "Linux":
-    LINK_ARGS = ["-Wl,-lhdf5,-lhdf5_cpp,-lhdf5_hl,-lhdf5_hl_cpp,-rpath,$ORIGIN"]
+    LINK_ARGS = ["-Wl,-rpath,$ORIGIN"]
     LIBRARY_DIRS = [
         "src/radarsimcpp/build",
         "src/radarsimcpp/hdf5/lib_linux_x86_64/lib",
     ]
-    LIBS = []
+    LIBS = ["hdf5", "hdf5_cpp", "hdf5_hl", "hdf5_hl_cpp"]
     if args.arch == "gpu":
         NVCC = "nvcc"
         CUDALIB = "lib64"
-        LIBS = ["cudart"]
+        LIBS = ["hdf5", "hdf5_cpp", "hdf5_hl", "hdf5_hl_cpp", "cudart"]
 elif os_type == "Darwin":
     LIBS = []
     if platform.processor() == "arm":
