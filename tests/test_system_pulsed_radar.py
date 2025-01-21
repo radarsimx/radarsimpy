e8268d6ddf25ec7b1e612fe361668477ccf46123
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2025-01-21 08:45:44 -0500

    Update test_system_pulsed_radar.py

--- tests/test_system_pulsed_radar.py
+++ tests/test_system_pulsed_radar.py
@@ -102,7 +102,7 @@ def test_pulsed_radar():
         np.arctan2(np.real(baseband[0, 0, :]), np.imag(baseband[0, 0, :])),
         np.array(
             [
-                0.0,
+                1.0,
                 0.0,
                 0.0,
                 0.0,
