cd4b929fe45cae5348e35d3cca3d3205c8b05b16
Zhengyu Peng <rookiepeng@users.noreply.github.com>
2025-07-12 23:31:08 -0400

    Update build.sh

--- build.sh
+++ build.sh
@@ -978,7 +978,6 @@ run_tests() {
         else
             log_error "$test_failures test suite(s) failed in ${test_time}s"
             log_error "CRITICAL: Test failures detected - build should fail"
-            set -e
             return $test_failures
         fi
     else
